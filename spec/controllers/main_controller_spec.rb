d8f1c61d6d80990ba4358560cb52c7bab7418acd
maxbarsukov <maximbarsukov@bk.ru>
2021-07-06 17:16:53 +0300

    Main controller spec added

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -3,4 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe MainController, type: :controller do
+  describe 'GET #index' do
+    let(:brands)  { create_list :brand, 3 }
+    let(:hits)    { create_list :product, 8 }
+
+    before { get :index }
+
+    context 'required output per page' do
+      it 'render to index template' do
+        is_expected.to render_template :index
+      end
+      it 'instance var brands include only brands' do
+        expect(assigns(:brands)).to match_array(brands)
+      end
+      it 'instance hits brands include only hits' do
+        expect(assigns(:hits)).to match_array(hits)
+      end
+    end
+  end
 end
