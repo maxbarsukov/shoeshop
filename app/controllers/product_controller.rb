3415d9b8810c469a462156d0b18b8f93a3baadae
maxbarsukov <maximbarsukov@bk.ru>
2021-07-09 18:48:34 +0300

    Product controller updated

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,16 @@
 class ProductController < ApplicationController
-  def show
+  before_action :set_product, only: %i[show set_page_options]
 
+  def show
+    set_page_options
+  end
+
+  def set_page_options
+    set_meta_tags @product.slice(:title, :description, :keywords)
+    add_breadcrumb 'Home', :root_path, title: 'Home'
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
