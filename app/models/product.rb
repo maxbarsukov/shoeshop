c13074149ff4efe7f9bb6abfc39835fc3b7b35cd
maxbarsukov <maximbarsukov@bk.ru>
2021-07-10 13:38:22 +0300

    Product assotiations ypdated

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
 
+  has_many :related_products
+  has_many :related, through: :related_products
+
   enum hit: { hit: 1, not_hit: 0 }
 
   def discount
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :price }
+
+  it { should have_many(:related_products) }
+  it { should have_many(:related) }
 end
