18c1ac71876813a45c7ef0f12d3b9ba3b8b2209b
maxbarsukov <maximbarsukov@bk.ru>
2021-07-10 14:40:05 +0300

    Specs updated for categories

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  it { should have_many(:products) }
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Product, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :price }
 
+  it { should belong_to(:category) }
+
   it { should have_many(:galleries) }
 
   it { should have_many(:related_products) }
