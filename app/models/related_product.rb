57060d622c43f84f88e6e19f6223b05a69b31ef1
maxbarsukov <maximbarsukov@bk.ru>
2021-07-10 13:35:14 +0300

    Related products updated

--- app/models/related_product.rb
+++ app/models/related_product.rb
@@ -1,2 +1,4 @@
 class RelatedProduct < ApplicationRecord
+  belongs_to :product
+  belongs_to :related, class_name: 'Product'
 end
--- spec/models/related_product_spec.rb
+++ spec/models/related_product_spec.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe RelatedProduct, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:product) }
+  it { should belong_to(:related) }
 end
