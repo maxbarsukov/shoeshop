0b4fd202cb3334ecbd88c4dd396e250c9bc9fff7
maxbarsukov <maximbarsukov@bk.ru>
2021-07-10 16:30:19 +0300

    Category controller updated

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,2 +1,11 @@
 class CategoryController < ApplicationController
+  def show
+    @category = Category.find(params[:id])
+    set_page_options
+  end
+
+  def set_page_options
+    set_meta_tags @category.slice(:title, :description, :keywords)
+    add_breadcrumb 'Home', :root_path, title: 'Home'
+  end
 end
