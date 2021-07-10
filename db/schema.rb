3cad30b6b7672c763ff2a94be0baa9a139689308
maxbarsukov <maximbarsukov@bk.ru>
2021-07-10 15:24:48 +0300

    Ancestry installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_112244) do
+ActiveRecord::Schema.define(version: 2021_07_10_115958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_07_10_112244) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "galleries", force: :cascade do |t|
