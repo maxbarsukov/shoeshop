81db3e56fc05db03a443882ba5fd646ff66e1885
maxbarsukov <maximbarsukov@bk.ru>
2021-07-11 14:55:43 +0300

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   resources :product,   only: [:show]
   resources :category,  only: [:show]
+  resources :search,    only: [:index]
 
   root to: 'main#index'
 end
