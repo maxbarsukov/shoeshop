66497fcda574ed12b7406d0118190d050e9c846f
maxbarsukov <maximbarsukov@bk.ru>
2021-07-11 16:43:10 +0300

    User-cart reference fixed

--- db/migrate/20210711132831_create_carts.rb
+++ db/migrate/20210711132831_create_carts.rb
@@ -1,7 +1,6 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-      add_reference :carts, :user, index: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_115958) do
+ActiveRecord::Schema.define(version: 2021_07_11_134207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,21 @@ ActiveRecord::Schema.define(version: 2021_07_10_115958) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -60,6 +75,12 @@ ActiveRecord::Schema.define(version: 2021_07_10_115958) do
   create_table "related_products", force: :cascade do |t|
     t.integer "product_id"
     t.integer "related_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
