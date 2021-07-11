91c299a1cc10e94ac39929d12cae7ba2c4e865a5
maxbarsukov <maximbarsukov@bk.ru>
2021-07-11 17:34:23 +0300

    User as devise model configured

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
   resources :product,   only: [:show]
   resources :category,  only: [:show]
   resources :search,    only: [:index]
