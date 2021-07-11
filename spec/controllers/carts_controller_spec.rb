b05ae400ce49dcad030f1c2ab58b271e326f4797
maxbarsukov <maximbarsukov@bk.ru>
2021-07-11 17:20:41 +0300

    Carts spec updated

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -4,4 +4,18 @@ require 'rails_helper'
 
 RSpec.describe CartsController, type: :controller do
   render_views
+
+  let!(:user) { create :user }
+  let(:product) { create :product }
+  let(:cart) { create :cart, user: user }
+  let!(:item) { create :cart_item, cart: cart, product: product }
+
+  describe 'GET #show' do
+    subject { get :show }
+
+    it 'render show view' do
+      is_expected.to render_template(:show)
+      expect(response.body).to include(product.title)
+    end
+  end
 end
