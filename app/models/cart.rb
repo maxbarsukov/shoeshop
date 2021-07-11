a91e40f67f86651b8f4ec3fc5c5b81331696ca70
maxbarsukov <maximbarsukov@bk.ru>
2021-07-11 16:54:19 +0300

    Cart model and spec updated

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,8 @@
 class Cart < ApplicationRecord
+  belongs_to :user
+
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: :cart_items
+
+  validates :user_id, presence: true
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -3,4 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+  it { should have_many(:cart_items) }
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:user_id) }
+  # it { should have_many(:products) }
 end
