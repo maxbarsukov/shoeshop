7922dff403ef3d5087e3b1157dc85c69c8f98890
maxbarsukov <maximbarsukov@bk.ru>
2021-07-11 14:10:17 +0300

    Spec for search controller added

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -3,4 +3,30 @@
 require 'rails_helper'
 
 RSpec.describe SearchController, type: :controller do
+  render_views
+
+  describe 'GET #index' do
+    let!(:product) { create(:product, title: 'product_for_searching') }
+    let(:query) { 'produ' }
+    subject! { get :index, params: { query: query, format: :json } }
+
+    context 'search product' do
+      it 'find product in db' do
+        res = JSON.parse(response.body).first.values
+        expect(res).to contain_exactly(product.title, product.id)
+      end
+
+      it 'response with 200 ok' do
+        expect(response.code).to eq(200)
+      end
+    end
+
+    context 'search with wrong query' do
+      let(:query) { 'wrong' }
+
+      it 'nothing found' do
+        expect response.body.empty?
+      end
+    end
+  end
 end
