2607ce59da6c47ebea696a9df0729d22e2eb5929
maxbarsukov <maximbarsukov@bk.ru>
2021-07-11 16:34:06 +0300

    Little fix in user-cart references

--- db/migrate/20210711132831_create_carts.rb
+++ db/migrate/20210711132831_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-      t.integer :user_id
+      add_reference :carts, :user, index: true
       t.timestamps
     end
   end
