96f609148526340cdb816a90a3a307ca25544f88
maxbarsukov <maximbarsukov@bk.ru>
2021-07-12 15:11:58 +0300

    User model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :cart, dependent: :destroy
+
+  validates :email,    presence: true
+  validates :password, presence: true
 end
