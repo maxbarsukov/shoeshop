270c00ca82ec4a10ef60c31fb62d591f7992ccd7
maxbarsukov <maximbarsukov@bk.ru>
2021-07-12 14:39:56 +0300

    RSpec config updated

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
 require 'shoulda/matchers'
 require 'rspec/json_expectations'
 require 'database_cleaner'
@@ -18,6 +19,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.include FactoryBot::Syntax::Methods
