e1d64f0bf8d309d16a066e63b0228b960f687c3f
maxbarsukov <maximbarsukov@bk.ru>
2021-07-12 14:50:05 +0300

    Cart spec updated

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -14,8 +14,28 @@ RSpec.describe CartsController, type: :controller do
     subject { get :show }
 
     it 'render show view' do
+      sign_in user
       is_expected.to render_template(:show)
       expect(response.body).to include(product.title)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    subject { delete :destroy }
+    context 'delete form cart' do
+      it 'delete product from cart' do
+        sign_in user
+        expect { subject }.to change { user.reload.cart.present? }.to(false)
+      end
+
+      it 'delete product from cart_items' do
+        sign_in user
+        expect { subject }.to change(user.cart.cart_items, :count).by(-1)
+      end
+
+      it 'render view show after destroy' do
+        is_expected.to render_template :show
+      end
     end
   end
 end
