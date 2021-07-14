04dd70b6e68f3fc6f0e6f2482335b8a93ce29d6b
maxbarsukov <maximbarsukov@bk.ru>
2021-07-14 11:57:26 +0300

    Typo fixed

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :facebook
 
   def facebook
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
