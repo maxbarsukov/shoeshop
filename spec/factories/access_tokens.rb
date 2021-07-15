a98f7bc3a96f0ae7b4e2b6b9c1f39001949814f1
maxbarsukov <maximbarsukov@bk.ru>
2021-07-15 14:10:58 +0300

    Some factories for doorkeeper added

---
+++ spec/factories/access_tokens.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :access_token, class: Doorkeeper::AccessToken do
+    application       { create(:oauth_application) }
+    resource_owner_id { create(:user).id }
+  end
+end
---
+++ spec/factories/oauth_applications.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :oauth_application, class: Doorkeeper::Application do
+    name          { 'Test' }
+    redirect_url  { 'urn:ietf:wg:oauth:2.0:oob' }
+    uid           { '12345678' }
+    secret        { '87654321' }
+  end
+end
