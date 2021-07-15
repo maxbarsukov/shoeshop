59aeb60d2d966bb10328f2f5c0f4397e65e545d8
maxbarsukov <maximbarsukov@bk.ru>
2021-07-15 13:49:12 +0300

    Product specs updated

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe ProductController, type: :controller do
 
   describe 'GET #show' do
     let(:product) { create :product }
+    let!(:user) { create :user }
 
     subject { get :show, params: { id: product.id } }
 
     context 'show product' do
       it 'render show view' do
+        sign_in user
         is_expected.to render_template :show
       end
     end
