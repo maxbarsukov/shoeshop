cd299290179a749b7a9f49b3f7000689d9c4b642
maxbarsukov <maximbarsukov@bk.ru>
2021-07-15 12:09:21 +0300

    Product policy spec updated

--- spec/policies/product_policy_spec.rb
+++ spec/policies/product_policy_spec.rb
@@ -1,27 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ProductPolicy, type: :policy do
-  let(:user) { User.new }
+  let(:user)  { create :user }
+  let(:admin) { create :user, admin: true }
 
   subject { described_class }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
   permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    context 'for auth user' do
+      it 'grants access if user admin' do
+        expect(subject).to permit(user)
+      end
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+      it 'grants access if user present' do
+        expect(subject).to permit(admin)
+      end
+    end
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context 'for a visitor' do
+      let(:user) { nil }
+      it { should_not permit(user) }
+    end
   end
 end
