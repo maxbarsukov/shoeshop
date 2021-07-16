2c6dce15bd14c339e291c7318daaf1d87bb23663
maxbarsukov <maximbarsukov@bk.ru>
2021-07-16 14:53:02 +0300

    N+1 problem fixed in api product controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::ProductsController < Api::V1::BaseApiController
   skip_before_action :doorkeeper_authorize!, on: :index
 
   def index
-    @products = Product.all.limit(5)
+    @products = Product
+                  .all
+                  .includes([:related, :category])
+                  .limit(5)
     respond_with @products
   end
 end
