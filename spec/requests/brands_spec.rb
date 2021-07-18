666425893d14707e595ad9491461c20b4ec2b258
maxbarsukov <maximbarsukov@bk.ru>
2021-07-18 13:33:13 +0300

    Brand spec request updated

--- spec/requests/brands_spec.rb
+++ spec/requests/brands_spec.rb
@@ -4,15 +4,19 @@ require 'rails_helper'
 
 RSpec.describe "Brands", type: :request do
   describe "GET /index" do
+    let(:brand) { create(:brand) }
+
     it "returns http success" do
-      get "/brands/index"
+      get brands_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
+    let(:brand) { create(:brand) }
+
     it "returns http success" do
-      get "/brands/show"
+      get brand_path(brand)
       expect(response).to have_http_status(:success)
     end
   end
