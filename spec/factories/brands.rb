df86532972fbd42401c4e18f6862e066db524edf
maxbarsukov <maximbarsukov@bk.ru>
2021-07-18 13:32:31 +0300

    Brand factory updated

--- spec/factories/brands.rb
+++ spec/factories/brands.rb
@@ -16,7 +16,7 @@ FactoryBot.define do
   factory :brand do
     title       { Faker::Lorem.sentence(word_count: 10) }
     bytitle     { Faker::Lorem.sentence(word_count: 10).downcase }
-    img         { Faker::Lorem.sentence(word_count: 10).downcase }
+    img         { 'ecco.jpg' }
     description { Faker::Lorem.sentence(word_count: 10) }
   end
 end
