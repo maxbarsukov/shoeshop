cbc7ff9bab807c022d2caea827be74abfbda50c6
maxbarsukov <maximbarsukov@bk.ru>
2021-07-20 14:56:17 +0300

    Meta-tags updated

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,16 +1,6 @@
 class MainController < ApplicationController
-  before_action :set_page_options
-
   def index
     @brands = Brand.limit(3)
     @hits   = Product.active.hit.limit(8)
-  end
-
-  private
-
-  def set_page_options
-    @page_title = 'Shoe Store'
-    @page_description = 'The Best Fake Shoe Store That Ever Existed'
-    @page_keywords = 'Shoe Store Man Woman Kids'
   end
 end
