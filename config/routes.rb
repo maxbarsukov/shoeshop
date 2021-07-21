12c15b324e9424b3a211972cfc984127fb3cfe90
maxbarsukov <maximbarsukov@bk.ru>
2021-07-21 14:36:33 +0300

    Routes annotations updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 # == Route Map
 #
 #                           Prefix Verb     URI Pattern                                                                              Controller#Action
+#                      rails_admin          /admin                                                                                   RailsAdmin::Engine
 #       native_oauth_authorization GET      /oauth/authorize/native(.:format)                                                        doorkeeper/authorizations#show
 #              oauth_authorization GET      /oauth/authorize(.:format)                                                               doorkeeper/authorizations#new
 #                                  DELETE   /oauth/authorize(.:format)                                                               doorkeeper/authorizations#destroy
@@ -56,6 +57,18 @@
 #               rails_disk_service GET      /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
 #        update_rails_disk_service PUT      /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
 #             rails_direct_uploads POST     /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+#
+# Routes for RailsAdmin::Engine:
+#   dashboard GET         /                                      rails_admin/main#dashboard
+#       index GET|POST    /:model_name(.:format)                 rails_admin/main#index
+#         new GET|POST    /:model_name/new(.:format)             rails_admin/main#new
+#      export GET|POST    /:model_name/export(.:format)          rails_admin/main#export
+# bulk_delete POST|DELETE /:model_name/bulk_delete(.:format)     rails_admin/main#bulk_delete
+# bulk_action POST        /:model_name/bulk_action(.:format)     rails_admin/main#bulk_action
+#        show GET         /:model_name/:id(.:format)             rails_admin/main#show
+#        edit GET|PUT     /:model_name/:id/edit(.:format)        rails_admin/main#edit
+#      delete GET|DELETE  /:model_name/:id/delete(.:format)      rails_admin/main#delete
+# show_in_app GET         /:model_name/:id/show_in_app(.:format) rails_admin/main#show_in_app
 
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
